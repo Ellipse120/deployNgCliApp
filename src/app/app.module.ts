e507cca67a4903f4e96b0c2e953b3be412c3ca6f
Lusai <ellipse120@gmail.com>
2017-08-22 13:29:53 +0800

    #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { IndexComponent } from '../app/body/index/index.component';
-import { AboutComponent } from '../app/body/about/about.component';
-import { ProductComponent } from '../app/body/product/product.component';
-import { CustomerComponent } from '../app/body/customer/customer.component';
+import { IndexComponent } from './body/index/index.component';
+import { AboutComponent } from './body/about/about.component';
+import { ProductComponent } from './body/product/product.component';
+import { CustomerComponent } from './body/customer/customer.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
